60da48620f8326ea961306d34343df14552a3125
WilmarDeML <carewaz23@hotmail.com>
2025-02-28 23:39:32 -0500

    add cors middleware

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 const persons = [
   { 
@@ -39,6 +40,7 @@ const formatWithBody = (tokens, req, res) => {
   return `${method} ${url} ${status} ${contentLength} - ${responseTime}ms ${body}`;
 };
 
+app.use(cors())
 app.use(express.json());
 app.use(morgan(formatWithBody));
 
